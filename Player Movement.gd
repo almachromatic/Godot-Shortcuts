34292a56929e8048e5bb08b4481bd4dfe32bd3b8
almachromatic <almachromatic@gmail.com>
2026-06-23 17:07:09 -0700

    Update Player Movement.gd

--- Player Movement.gd
+++ Player Movement.gd
@@ -5,10 +5,10 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
 
