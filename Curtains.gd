8a88d36819fb474a1404816e304afdd96ca0b1e3
almachromatic <almachromatic@gmail.com>
2026-07-09 19:08:48 -0700

    Create Curtains.gd
    Open and close curtains

---
+++ Curtains.gd
@@ -0,0 +1,21 @@
+
+
+extends StaticBody2D
+
+@onready var interactable: Area2D = $Interactable
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	interactable.interact = _on_interact
+
+
+func _on_interact():
+	if sprite_2d.frame == 0:
+		sprite_2d.frame = 1
+		interactable.is_interactable = false
+		print("You closed the curtains")
+	
+	else:
+		sprite_2d.frame = 0
+		print("You opened the curtains")
