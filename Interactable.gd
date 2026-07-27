c5096eeb77b9715b055c3b5ba2ad73ebf2cea1bf
almachromatic <almachromatic@gmail.com>
2026-07-27 10:27:02 -0700

    Update Interactable.gd

--- Interactable.gd
+++ Interactable.gd
@@ -3,6 +3,11 @@ extends Area2D
 @export var interact_name: String = ""
 @export var is_interactable: bool = true
 
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
 var interact: Callable = func():
 	pass
 
+func action() -> void:
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start) #_example_dialogue_balloon can be replaced by actual dialogue baloon
